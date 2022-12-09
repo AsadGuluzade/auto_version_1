a025ab955663bd4ec75d560d4e1665eee19d876a
AsadGuluzade <guluzade.asad@gmail.com>
2022-12-09 15:40:22 +0000

    testing dinamic values

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: `Coyotiv's Express + Mongo starter Template` })
+  res.render('index', { title: `Coyotiv's Express + Mongo starter Template`, Onur: 'This is message' })
 })
 
 module.exports = router
